c68a3f5073973904162d314f346743ef83bdecfe
Bastien HUGUES <bastien.hugues@tb-it.fr>
2021-06-13 12:54:36 +0200

    Update right-function.php

--- right-function.php
+++ right-function.php
@@ -1,53 +1,55 @@
-function permUnit($domaine, $name){
+<?php
+	function permUnit($domaine, $name){
 
-	if($_SESSION['perm-'.$domaine.'-'.$name] == 'true'){return true;}else{return false;}
+		if($_SESSION['perm-'.$domaine.'-'.$name] == 'true'){return true;}else{return false;}
 
-}
+	}
 
-function permReq($domaine, $read, $write, $delete, $admin){
+	function permReq($domaine, $read, $write, $delete, $admin){
 
-	session_start();
-	include('bdd.php');
+		session_start();
+		include('bdd.php');
 
-	if($domaine == 'admin'){
+		if($domaine == 'admin'){
 
-		if($_SESSION['adminAll'] == 'true'){
+			if($_SESSION['adminAll'] == 'true'){
 
-			return true;
+				return true;
+
+			}else{
+
+				$m = $bdd->query('SELECT * FROM Offres');
+				while($mr = $m->fetch()){
+
+				    if($_SESSION['perm-'.$mr['Ref'].'-admin'] == 'true'){
+
+					return true;
+
+				    }
+
+				}
+
+			}
 
 		}else{
 
 			$m = $bdd->query('SELECT * FROM Offres');
-	        while($mr = $m->fetch()){
+		    	while($mr = $m->fetch()){
 
-	            if($_SESSION['perm-'.$mr['Ref'].'-admin'] == 'true'){
+				if($mr['Ref'] == $domaine){
 
-	            	return true;
+					if($read == 'true'){if(!permUnit($domaine, 'read')){return false;}}
+					if($write == 'true'){if(!permUnit($domaine, 'write')){return false;}}
+					if($delete == 'true'){if(!permUnit($domaine, 'delete')){return false;}}
+					if($admin == 'true'){if(!permUnit($domaine, 'admin')){return false;}}
 
-	            }
+					return true;
 
-			}
+				}
+
+		    	}
 
 		}
 
-	}else{
-
-		$m = $bdd->query('SELECT * FROM Offres');
-	    while($mr = $m->fetch()){
-
-	    	if($mr['Ref'] == $domaine){
-
-	    		if($read == 'true'){if(!permUnit($domaine, 'read')){return false;}}
-				  if($write == 'true'){if(!permUnit($domaine, 'write')){return false;}}
-				  if($delete == 'true'){if(!permUnit($domaine, 'delete')){return false;}}
-				  if($admin == 'true'){if(!permUnit($domaine, 'admin')){return false;}}
-
-				  return true;
-
-	    	}
-
-	    }
-
 	}
-
-}
+<?
